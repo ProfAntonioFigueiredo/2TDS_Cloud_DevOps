e1143e80cfd43dd23d1b46768879f0dd0940064a
Antonio Figueiredo <antonio.figueiredo@wiseticonsulting.com>
2023-03-20 17:31:59 -0300

    Desafio Microsoft - Aulas 20 Março

---
+++ Desafio_Microsoft/program.py
@@ -0,0 +1,44 @@
+# program.py
+
+# Exec 1
+#sum = 1 + 2
+#print(sum)
+
+# Exec 2
+#print("Show this in the Console")
+
+# Exec 3 - Variáveis 
+#sum = 1 + 2 # 3
+#product = sum * 2
+#print(product)
+
+# Exec 4 Tipos de Dados
+# planets_in_solar_system = 8 #int, pluto used to be 9th planet, but is too small
+# distance_to_alpha_centuri = 4.367 # float, lightyears
+# can_liftoff = True
+# shuttle_landed_on_the_moon = "Apolo 11" #string
+
+# Exec 5 Operadores
+# left_side = 10
+# right_side = 5
+# left_side / right_side # 2
+
+# Exec 5 Operadores de atribuição 
+# =	x = 2
+# x agora contém 2.
+
+# +=	x += 2
+# x incrementado em 2. Se ele continha 2 antes, agora ele terá valor de 4.
+
+# -=	x -= 2
+# x decrementado em 2. Se ele continha 2 antes, agora ele terá valor de 0.
+
+# /=	x /= 2
+# x dividido por 2. Se ele continha 2 antes, agora ele terá valor de 1.
+
+# *=	x *= 2
+# x multiplicado por 2. Se ele continha 2 antes, agora ele terá valor de 4.
+
+#Exec 6 Datas
+from datetime import date
+date.today()

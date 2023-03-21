975f2c731813166b36b9edbe39683d33591a9839
Antonio Figueiredo <antonio.figueiredo@wiseticonsulting.com>
2023-03-21 19:22:50 -0300

    Update project Folder

---
+++ Desafio_Microsoft/React_Snopack2/code/0-starter/src/App.jsx
@@ -0,0 +1,15 @@
+import './index.css'
+import RecipeTitle from './RecipeTitle'
+import React from 'react';
+
+
+function App() {
+    return (
+        <article>
+            <h1>Recipe Manager</h1>
+            <RecipeTitle />
+        </article>
+    )
+}
+
+export default App;
